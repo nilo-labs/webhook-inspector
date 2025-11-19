552a4ad71d2ec326e393779138914e1581abf431
danilotavares-dev <danilotavarespr00@gmail.com>
2025-11-18 22:47:03 -0300

    feat(web): Adiciona lógica e componentes para listagem visual de webhooks

--- web/src/components/webhooks-list-item.tsx
+++ web/src/components/webhooks-list-item.tsx
@@ -4,6 +4,7 @@ import { formatDistanceToNow } from 'date-fns'
 import { Trash2Icon } from 'lucide-react'
 import { Checkbox } from './ui/checkbox'
 import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
 
 interface WebhooksListItemProps {
   webhook: {
@@ -12,9 +13,11 @@ interface WebhooksListItemProps {
     pathname: string
     createdAt: Date
   }
+  onWebhookChecked: (webhookId: string) => void
+  isWebhookChecked: boolean
 }
 
-export function WebhooksListItem({ webhook }: WebhooksListItemProps) {
+export function WebhooksListItem({ webhook, onWebhookChecked, isWebhookChecked }: WebhooksListItemProps) {
   const QueryClient = useQueryClient()
 
   const { mutate: deleteWebhook } = useMutation ({
@@ -33,7 +36,10 @@ export function WebhooksListItem({ webhook }: WebhooksListItemProps) {
   return (
     <div className="group rounded-lg transition-colors duration-150 hover:bg-zinc-700/30">
       <div className="flex items-start gap-3 px-4 py-2.5">
-        <Checkbox />
+        <Checkbox
+          onCheckedChange={() => onWebhookChecked(webhook.id)}
+          checked={isWebhookChecked}
+        />
 
         <Link
           to="/webhooks/$id"
--- web/src/components/webhooks-list.tsx
+++ web/src/components/webhooks-list.tsx
@@ -1,15 +1,21 @@
-import { Loader, Loader2 } from 'lucide-react'
+import { Loader2, Wand2 } from 'lucide-react'
+import * as Dialog from '@radix-ui/react-dialog'
 import { webhooksListSchema } from '../http/schemas/webhooks'
 import { WebhooksListItem } from './webhooks-list-item'
 import {
   useSuspenseInfiniteQuery,
   useSuspenseQuery,
 } from '@tanstack/react-query'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { string } from 'zod'
+import { CodeBlock } from './ui/code-block'
 
 export function WebhookList() {
   const loadMoreRef = useRef<HTMLDivElement>(null)
   const observerRef = useRef<IntersectionObserver>(null)
+
+  const [checkedWebhooksIds, setCheckedWebhooksIds] = useState<string[]>([])
+  const [generateHandlerCode, setGenerateHandlerCode] = useState<string | null>(null)
 
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useSuspenseInfiniteQuery({
@@ -33,7 +39,6 @@ export function WebhookList() {
       initialPageParam: undefined as string | undefined,
     })
 
-  const webhooks = data.pages.flatMap((page) => page.webhooks)
 
   useEffect(() => {
     if (observerRef.current) {
@@ -64,23 +69,81 @@ export function WebhookList() {
     }
   }, [hasNextPage, isFetchingNextPage, fetchNextPage])
 
+  function handleCheckWebhook(checkedWebhooksId: string) {
+    if (checkedWebhooksIds.includes(checkedWebhooksId)) {
+      setCheckedWebhooksIds(state => {
+        return state.filter(webhookId => webhookId !== checkedWebhooksId)
+      })
+    } else {
+      setCheckedWebhooksIds(state => [...state, checkedWebhooksId])
+    }
+  }
+
+  async function handleGenerateHandler() {
+    const response = await fetch('http://localhost:3333/api/generate', {
+      method: 'POST',
+      body: JSON.stringify({ webhookIds: checkedWebhooksIds }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+
+    type GenerateResponse = { code: string }
+
+    const data: GenerateResponse = await response.json()
+
+    setGenerateHandlerCode(data.code)
+  }
+
+  const hasAnyWebhookChecked = checkedWebhooksIds.length > 0
+
   return (
-    <div className="flex-1 overflow-y-auto">
-      <div className="space-y-1 p-2">
-        {webhooks.map((webhook) => {
-          return <WebhooksListItem key={webhook.id} webhook={webhook} />
-        })}
+    <>
+      <div className="flex-1 overflow-y-auto">
+        <div className="space-y-1 p-2">
+          <button
+            disabled={!hasAnyWebhookChecked}
+            className='w-full  bg-indigo-700 mb-3 text-white size-8 rounded-lg flex items-center justify-center gap-3 font-medium text-sm py-2.5 disabled:opacity-50'
+            onClick={() => handleGenerateHandler()}
+          >
+            <Wand2 className='size-4' />
+            Gerar handler
+          </button>
+
+          {(data.pages.flatMap((page) => page.webhooks)).map((webhook) => {
+            return (
+              <WebhooksListItem
+                key={webhook.id}
+                webhook={webhook}
+                onWebhookChecked={handleCheckWebhook}
+                isWebhookChecked={checkedWebhooksIds.includes(webhook.id)}
+              />
+            )
+          })}
+        </div>
+
+        {hasNextPage && (
+          <div className="p-2" ref={loadMoreRef}>
+            {isFetchingNextPage && (
+              <div className="flex items-center justify-center py-2">
+                <Loader2 className="size-5 animate-spin text-zinc-500" />
+              </div>
+            )}
+          </div>
+        )}
       </div>
 
-      {hasNextPage && (
-        <div className="p-2" ref={loadMoreRef}>
-          {isFetchingNextPage && (
-            <div className="flex items-center justify-center py-2">
-              <Loader2 className="size-5 animate-spin text-zinc-500" />
+      {!!generateHandlerCode && (
+        <Dialog.Root defaultOpen>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed z-20" />
+
+          <Dialog.Content className="flex items-center justify-center fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 z-40">
+            <div className='bg-zinc-900 w-[600px] p-4 rounded-lg border border-zinc-800 max-h-[620px] overflow-y-auto'>
+              <CodeBlock language="typescript" code={generateHandlerCode} />
             </div>
-          )}
-        </div>
+          </Dialog.Content>
+        </Dialog.Root>
       )}
-    </div>
+    </>
   )
 }
